69cdb023adac6249f04e7436065fbe3bdc699bce
Xinjian OUYANG <xinjian.ouyang@student-cs.fr>
2021-03-14 11:29:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import u_net
 
 unet = u_net.U_Net()
 # model_path = r"weights/best_model.h5" # the model we have trained
-# unet.test(model_path)     # test
+# unet.test(model_path)     # test results are saved in folder evaluation
 
 # unet.test1()  # test randomly
 
