c0f5375caf71083eb0a4ca92243f2f7879067ec7
Xinjian OUYANG <xinjian.ouyang@student-cs.fr>
2021-03-14 12:03:53 +0100

    update

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import u_net
 
 unet = u_net.U_Net()
 
-# model_path = r"weights/history/best_model.h5" # the model we have trained
-# unet.test(model_path)     # test results are saved in folder evaluation
+model_path = r"weights/history/model.h5" # the model we have trained
+unet.test(model_path)     # test results are saved in folder evaluation
 
 # unet.test1()  # test randomly
-save_path = 'weights/history'
-unet.train(epochs = 2, save_path = save_path)    # trainned model is saved in folder weights/history
+# save_path = 'weights/history'
+# unet.train(epochs = 2, save_path = save_path)    # trainned model is saved in folder weights/history
 
 
 
